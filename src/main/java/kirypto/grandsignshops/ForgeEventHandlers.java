40509abe27ffdfb50f930cb892880047c6ae8236
kirypto <kirypto@gmail.com>
2019-06-09 17:43:12 -0600

    Got mod working with Grand Economy API, clicking sign with 'password' earns 42.

---
+++ src/main/java/kirypto/grandsignshops/ForgeEventHandlers.java
@@ -0,0 +1,66 @@
+package kirypto.grandsignshops;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockSign;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntitySign;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import the_fireplace.grandeconomy.GrandEconomy;
+import the_fireplace.grandeconomy.api.GrandEconomyApi;
+
+import static java.lang.String.format;
+
+public class ForgeEventHandlers {
+
+    @SubscribeEvent
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+        if (!(event instanceof PlayerInteractEvent.LeftClickBlock) && !(event instanceof PlayerInteractEvent.RightClickBlock)) {
+            return;
+        }
+
+        EntityPlayer player = event.getEntityPlayer();
+        World world = event.getWorld();
+        BlockPos pos = event.getPos();
+        Block block = world.getBlockState(pos).getBlock();
+        String clickType = (event instanceof PlayerInteractEvent.LeftClickBlock) ? "left" : "right";
+
+
+        if (!(block instanceof BlockSign)) {
+            sendPlayerMessage(player, "Not a sign block.");
+            return;
+        }
+
+        TileEntity tileEntity = world.getTileEntity(pos);
+        if (!(tileEntity instanceof TileEntitySign)) {
+            sendPlayerMessage(player, "Not a sign entity.");
+            return;
+        }
+
+        TileEntitySign tileEntitySign = (TileEntitySign) tileEntity;
+
+        String fullSignText = Arrays.stream(tileEntitySign.signText)
+                .map(iTextComp -> format("[%s, %s]", iTextComp.getFormattedText(), iTextComp.getUnformattedComponentText()))
+                .collect(Collectors.joining());
+
+        if (fullSignText.contains("password")) {
+            int earnings = 42;
+            sendPlayerMessage(player, format("You just %s clicked on a sign with the pass phrase, earning %s!", clickType, earnings));
+            GrandEconomyApi.addToBalance(player.getUniqueID(), earnings, true);
+        } else {
+            sendPlayerMessage(player, format("The sign did not have the pass phrase... It just said '%s'", fullSignText));
+        }
+    }
+
+    private void sendPlayerMessage(EntityPlayer player, String message) {
+        player.sendMessage(new TextComponentString(message));
+    }
+}
--- src/main/java/kirypto/grandsignshops/GrandSignShops.java
+++ src/main/java/kirypto/grandsignshops/GrandSignShops.java
@@ -3,7 +3,10 @@ package kirypto.grandsignshops;
 import net.minecraft.command.ICommandManager;
 import net.minecraft.command.ServerCommandManager;
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = GrandSignShops.MOD_ID, name = GrandSignShops.MODNAME, version = GrandSignShops.VERSION,
@@ -16,10 +19,17 @@ public final class GrandSignShops {
     @Mod.Instance(GrandSignShops.MOD_ID)
     public static GrandSignShops instance;
 
-    @Mod.EventHandler
+    @EventHandler
     public void onServerStart(FMLServerStartingEvent event) {
         MinecraftServer server = event.getServer();
         ICommandManager command = server.getCommandManager();
         ServerCommandManager manager = (ServerCommandManager) command;
+
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers());
     }
 }
