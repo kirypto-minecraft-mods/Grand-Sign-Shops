beba145ba065b3ce65179c08ec7c0782d1072523
kirypto <kirypto@gmail.com>
2019-06-09 18:05:10 -0600

    Changed approach version number to use alpha suffix

--- src/main/java/kirypto/grandsignshops/GrandSignShops.java
+++ src/main/java/kirypto/grandsignshops/GrandSignShops.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public final class GrandSignShops {
     public static final String MOD_ID = "grandsignshops";
     public static final String MODNAME = "Grand Sign Shops";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.0-alpha2";
 
     @Mod.Instance(GrandSignShops.MOD_ID)
     public static GrandSignShops instance;
