4a26f985aca31e9f67abee4d535d527328ae80ad
kirypto <kirypto@gmail.com>
2019-06-23 15:46:43 -0600

    Added save, retrieveByPlayer, and clearByPlayer methods to the UnclosedCommandRepository interface

--- src/main/java/kirypto/grandsignshops/Repository/InMemoryUnclosedCommandRepository.java
+++ src/main/java/kirypto/grandsignshops/Repository/InMemoryUnclosedCommandRepository.java
@@ -1,5 +1,27 @@
 package kirypto.grandsignshops.Repository;
 
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import kirypto.grandsignshops.UnclosedShopCommand;
+
 public class InMemoryUnclosedCommandRepository implements UnclosedCommandRepository {
     public InMemoryUnclosedCommandRepository() { }
+
+    @Override
+    public void save(UnclosedShopCommand unclosedShopCommand) {
+        throw new NotImplementedException("");
+    }
+
+    @Override
+    public Optional<UnclosedShopCommand> retrieveByPlayer(UUID playerUniqueId) {
+        throw new NotImplementedException("");
+    }
+
+    @Override
+    public void clearByPlayer(UUID playerUniqueId) {
+        throw new NotImplementedException("");
+    }
 }
--- src/main/java/kirypto/grandsignshops/Repository/UnclosedCommandRepository.java
+++ src/main/java/kirypto/grandsignshops/Repository/UnclosedCommandRepository.java
@@ -1,4 +1,14 @@
 package kirypto.grandsignshops.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import kirypto.grandsignshops.UnclosedShopCommand;
+
 public interface UnclosedCommandRepository {
+    void save(UnclosedShopCommand unclosedShopCommand);
+
+    Optional<UnclosedShopCommand> retrieveByPlayer(UUID playerUniqueId);
+
+    void clearByPlayer(UUID playerUniqueId);
 }
