99405835d7b241788f573fbb267f1635ee1e266e
kirypto <kirypto@gmail.com>
2019-06-24 14:30:46 -0600

    Changed param argument type of UnclosedShopCommand to map from enum to object

---
+++ src/main/java/kirypto/grandsignshops/UnclosedCommandParam.java
@@ -0,0 +1,10 @@
+package kirypto.grandsignshops;
+
+public enum UnclosedCommandParam {
+    ITEM,
+    META,
+    BUY_LOW,
+    BUY_HIGH,
+    SELL_LOW,
+    SELL_HIGH
+}
--- src/main/java/kirypto/grandsignshops/UnclosedShopCommand.java
+++ src/main/java/kirypto/grandsignshops/UnclosedShopCommand.java
@@ -1,28 +1,29 @@
 package kirypto.grandsignshops;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
-
-import java.time.Instant;
 
 public class UnclosedShopCommand {
     private final UnclosedShopCommandType unclosedShopCommandType;
     private final UUID playerUniqueId;
     private final Instant creationTime;
-    private final Map<String, Integer> params;
+    private final Map<UnclosedCommandParam, Object> params;
 
     private UnclosedShopCommand(
             UnclosedShopCommandType unclosedShopCommandType,
             UUID playerUniqueId,
-            Map<String, Integer> params) {
+            Map<UnclosedCommandParam, Object> params) {
         this.unclosedShopCommandType = unclosedShopCommandType;
         this.playerUniqueId = playerUniqueId;
         this.params = params;
         this.creationTime = Instant.now();
     }
 
-    public static UnclosedShopCommand of(UnclosedShopCommandType unclosedShopCommandType, UUID playerUniqueId, Map<String, Integer> params) {
+    public static UnclosedShopCommand of(
+            UnclosedShopCommandType unclosedShopCommandType,
+            UUID playerUniqueId,
+            Map<UnclosedCommandParam, Object> params) {
         return new UnclosedShopCommand(unclosedShopCommandType, playerUniqueId, params);
     }
 }
-
