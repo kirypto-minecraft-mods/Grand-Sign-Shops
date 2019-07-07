51a45b7e83bb2e738d1d88390b62c6b3b69c7997
kirypto <kirypto@gmail.com>
2019-07-07 09:57:16 -0600

    Created a PlayerSignInteractionType enum

---
+++ src/main/java/kirypto/grandsignshops/PlayerSignInteractionType.java
@@ -0,0 +1,11 @@
+package kirypto.grandsignshops;
+
+public enum PlayerSignInteractionType {
+    LEFT_CLICK,
+    RIGHT_CLICK;
+
+    @Override
+    public String toString() {
+        return name();
+    }
+}
