0515b52f9def42fac2c265c013df9ef7a46f2a15
kirypto <kirypto@gmail.com>
2019-08-02 17:26:08 -0600

    Added expected necessary methods to GrandSignShopRepository

--- src/main/java/kirypto/grandsignshops/Repository/GrandSignShopRepository.java
+++ src/main/java/kirypto/grandsignshops/Repository/GrandSignShopRepository.java
@@ -1,4 +1,13 @@
 package kirypto.grandsignshops.Repository;
 
+import java.util.Optional;
+
+import kirypto.grandsignshops.GrandSignShop;
+
 public interface GrandSignShopRepository {
+    void create(GrandSignShop grandSignShop);
+
+    Optional<GrandSignShop> retrieve(BlockLocation blockLocation);
+
+    void delete(BlockLocation blockLocation);
 }
--- src/main/java/kirypto/grandsignshops/Repository/JsonGrandSignShopRepository.java
+++ src/main/java/kirypto/grandsignshops/Repository/JsonGrandSignShopRepository.java
@@ -1,12 +1,31 @@
 package kirypto.grandsignshops.Repository;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import java.io.File;
+import java.util.Optional;
+
+import kirypto.grandsignshops.GrandSignShop;
 
 public class JsonGrandSignShopRepository implements GrandSignShopRepository {
     private final File grandSignShopsRootFolder;
 
     public JsonGrandSignShopRepository(File grandSignShopsRootFolder) {
-
         this.grandSignShopsRootFolder = grandSignShopsRootFolder;
+    }
+
+    @Override
+    public void create(GrandSignShop grandSignShop) {
+        throw new NotImplementedException("");
+    }
+
+    @Override
+    public Optional<GrandSignShop> retrieve(BlockLocation blockLocation) {
+        throw new NotImplementedException("");
+    }
+
+    @Override
+    public void delete(BlockLocation blockLocation) {
+        throw new NotImplementedException("");
     }
 }
