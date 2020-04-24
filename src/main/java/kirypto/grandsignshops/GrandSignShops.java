4e473ed6e220f5664c67faa9ab5583d2c34fffca
kirypto <kirypto@gmail.com>
2020-04-23 18:36:20 -0600

    Update project config to work with MC 1.15.2

--- src/main/java/kirypto/grandsignshops/GrandSignShops.java
+++ src/main/java/kirypto/grandsignshops/GrandSignShops.java
@@ -1,62 +1,70 @@
 package kirypto.grandsignshops;
 
-import net.minecraft.command.ICommandManager;
-import net.minecraft.command.ServerCommandManager;
+import net.minecraft.command.Commands;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.storage.ISaveHandler;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.storage.SaveHandler;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.util.Optional;
 
-import kirypto.grandsignshops.CommandHandlers.MainCommandHandler;
-import kirypto.grandsignshops.Events.ForgeEventHandlers;
-import kirypto.grandsignshops.Repository.GrandSignShopRepository;
-import kirypto.grandsignshops.Repository.InMemoryUnclosedCommandRepository;
-import kirypto.grandsignshops.Repository.JsonGrandSignShopRepository;
-import kirypto.grandsignshops.Repository.UnclosedCommandRepository;
-
-@Mod(modid = GrandSignShops.MOD_ID, name = GrandSignShops.MODNAME, version = GrandSignShops.VERSION,
-        acceptedMinecraftVersions = "1.12.2", acceptableRemoteVersions = "*")
+@Mod(GrandSignShops.MOD_ID)
 public final class GrandSignShops {
     public static final String MOD_ID = "grandsignshops";
     public static final String MODNAME = "Grand Sign Shops";
     public static final String VERSION = "1.0.0-alpha2";
+    private final Logger log;
 
-    @Mod.Instance(GrandSignShops.MOD_ID)
-    public static GrandSignShops instance;
-
-    @SideOnly(value = Side.SERVER)
-    @EventHandler
-    public void onServerStart(FMLServerStartingEvent event) {
-        System.out.println("@@@@@@@@@@@@@@@@@@@@ On Server Start");
-        MinecraftServer server = event.getServer();
-        ICommandManager command = server.getCommandManager();
-        ServerCommandManager manager = (ServerCommandManager) command;
-
-        getWorldSaveFolder(server)
-                .map(worldSaveFolder -> new File(worldSaveFolder, "GrandSignShops"))
-                .ifPresent(grandSignShopsRootFolder -> {
-                    GrandSignShopRepository grandSignShopRepository = new JsonGrandSignShopRepository(grandSignShopsRootFolder);
-                    UnclosedCommandRepository unclosedCommandRepository = new InMemoryUnclosedCommandRepository();
-
-                    manager.registerCommand(new MainCommandHandler(unclosedCommandRepository));
-                    MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers(unclosedCommandRepository, grandSignShopRepository));
-                });
+    public GrandSignShops() {
+        log = LogManager.getLogger();
     }
 
-    private Optional<File> getWorldSaveFolder(MinecraftServer server) {
-        ISaveHandler handler = server.getWorld(DimensionType.OVERWORLD.getId()).getSaveHandler();
-        if (!(handler instanceof SaveHandler)) {
-            return Optional.empty();
-        }
-        return Optional.of(handler.getWorldDirectory());
+    // @Mod.Instance(GrandSignShops.MOD_ID)
+    // public static GrandSignShops instance;
+
+    // @SideOnly(value = Side.SERVER)
+    // @EventHandler
+    public void onServerStart(FMLDedicatedServerSetupEvent event) {
+        // System.out.println("@@@@@@@@@@@@@@@@@@@@ On Server Start");
+        DedicatedServer dedicatedServer = event.getServerSupplier().get();
+        // MinecraftServer server = event.getServer();
+        Commands commandManager = dedicatedServer.getCommandManager();
+        // ICommandManager command = server.getCommandManager();
+
+        // ServerCommandManager manager = (ServerCommandManager) command;
+
+        File worldSaveFolder = getWorldSaveFolder(dedicatedServer);
+        File grandSignShopsRootFolder = new File(worldSaveFolder, "GrandSignShops");
+        // getWorldSaveFolder(server)
+        //         .map(worldSaveFolder -> new File(worldSaveFolder, "GrandSignShops"))
+        //         .ifPresent(grandSignShopsRootFolder -> {
+        // GrandSignShopRepository grandSignShopRepository = new JsonGrandSignShopRepository(grandSignShopsRootFolder);
+        // UnclosedCommandRepository unclosedCommandRepository = new InMemoryUnclosedCommandRepository();
+
+        // commandManager.getDispatcher().register(new LiteralArgumentBuilder<>()).registerCommand(new MainCommandHandler(unclosedCommandRepository));
+        // MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers(unclosedCommandRepository, grandSignShopRepository));
+        //         });
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        log.info("onServerStarting");
+        event.getServer().getCommandManager().getDispatcher().register(Commands.literal("import").executes(command -> {
+            log.info("/import command dispatched");
+            //importScreen.show();
+            return 0;
+        }));
+    }
+
+    private File getWorldSaveFolder(MinecraftServer server) {
+        SaveHandler saveHandler = server.getWorld(DimensionType.OVERWORLD).getSaveHandler();
+        return saveHandler.getWorldDirectory();
     }
 }
