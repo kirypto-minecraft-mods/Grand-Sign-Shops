23c3873bb3968a5830b4ad51a6a6efd4ce14444d
kirypto <kirypto@gmail.com>
2020-04-27 20:54:40 -0600

    Uncomment GrandSignShopRepository code

--- src/main/java/kirypto/grandsignshops/domain/repository/GrandSignShopRepository.java
+++ src/main/java/kirypto/grandsignshops/domain/repository/GrandSignShopRepository.java
@@ -1,9 +1,14 @@
 package kirypto.grandsignshops.domain.repository;
 
+import java.util.Optional;
+
+import kirypto.grandsignshops.domain.BlockLocation;
+import kirypto.grandsignshops.domain.GrandSignShop;
+
 public interface GrandSignShopRepository {
-    // void create(GrandSignShop grandSignShop);
-    //
-    // Optional<GrandSignShop> retrieve(BlockLocation blockLocation);
-    //
-    // void delete(BlockLocation blockLocation);
+    void create(GrandSignShop grandSignShop);
+
+    Optional<GrandSignShop> retrieve(BlockLocation blockLocation);
+
+    void delete(BlockLocation blockLocation);
 }
