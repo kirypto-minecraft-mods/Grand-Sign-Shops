f01866421df04e18f5dfdf5d6ff743d8cbbfe376
kirypto <kirypto@gmail.com>
2020-04-27 21:02:34 -0600

    Uncomment JsonGrandSignShopRepository code and add todo for persisting

--- src/main/java/kirypto/grandsignshops/domain/repository/JsonGrandSignShopRepository.java
+++ src/main/java/kirypto/grandsignshops/domain/repository/JsonGrandSignShopRepository.java
@@ -2,44 +2,47 @@ package kirypto.grandsignshops.domain.repository;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import kirypto.grandsignshops.domain.BlockLocation;
 import kirypto.grandsignshops.domain.GrandSignShop;
 
 public class JsonGrandSignShopRepository implements GrandSignShopRepository {
-    // private final File grandSignShopsRootFolder;
-    // private final Map<BlockLocation, GrandSignShop> signLocationToGrandSignShopLocation;
-    // private final Map<BlockLocation, BlockLocation> allGrandSignShopLocationsToCorrespondingSignLocation;
-    //
-    // public JsonGrandSignShopRepository(File grandSignShopsRootFolder) {
-    //     this.grandSignShopsRootFolder = grandSignShopsRootFolder;
-    //
-    //     signLocationToGrandSignShopLocation = new HashMap<>();
-    //     allGrandSignShopLocationsToCorrespondingSignLocation = new HashMap<>();
-    // }
-    //
+    private final File grandSignShopsRootFolder;
+    private final Map<BlockLocation, GrandSignShop> signLocationToGrandSignShopLocation;
+    private final Map<BlockLocation, BlockLocation> allGrandSignShopLocationsToCorrespondingSignLocation;
+
+    public JsonGrandSignShopRepository(File grandSignShopsRootFolder) {
+        this.grandSignShopsRootFolder = grandSignShopsRootFolder;
+
+        signLocationToGrandSignShopLocation = new HashMap<>();
+        allGrandSignShopLocationsToCorrespondingSignLocation = new HashMap<>();
+    }
+
     @Override
     public void create(GrandSignShop grandSignShop) {
-    //     BlockLocation signLocation = grandSignShop.getSignLocation();
-    //     signLocationToGrandSignShopLocation.put(signLocation, grandSignShop);
-    //     allGrandSignShopLocationsToCorrespondingSignLocation.put(grandSignShop.getChestLocation(), signLocation);
-    //     for (BlockLocation additionalProtectedLocation : grandSignShop.getAdditionalProtectedLocations()) {
-    //         allGrandSignShopLocationsToCorrespondingSignLocation.put(additionalProtectedLocation, signLocation);
-    //     }
+        BlockLocation signLocation = grandSignShop.getSignLocation();
+        signLocationToGrandSignShopLocation.put(signLocation, grandSignShop);
+        allGrandSignShopLocationsToCorrespondingSignLocation.put(grandSignShop.getChestLocation(), signLocation);
+        for (BlockLocation additionalProtectedLocation : grandSignShop.getAdditionalProtectedLocations()) {
+            allGrandSignShopLocationsToCorrespondingSignLocation.put(additionalProtectedLocation, signLocation);
+        }
+        // TODO kirypto 2020-Apr-27: Persist to grandSignShopsRootFolder
     }
 
     @Override
     public Optional<GrandSignShop> retrieve(BlockLocation blockLocation) {
-        throw new NotImplementedException("");
-    //     if (signLocationToGrandSignShopLocation.containsKey(blockLocation)) {
-    //         return Optional.of(signLocationToGrandSignShopLocation.get(blockLocation));
-    //     }
-    //     if (allGrandSignShopLocationsToCorrespondingSignLocation.containsKey(blockLocation)) {
-    //         BlockLocation signLocation = allGrandSignShopLocationsToCorrespondingSignLocation.get(blockLocation);
-    //         return Optional.of(signLocationToGrandSignShopLocation.get(signLocation));
-    //     }
-    //     return Optional.empty();
+        if (signLocationToGrandSignShopLocation.containsKey(blockLocation)) {
+            return Optional.of(signLocationToGrandSignShopLocation.get(blockLocation));
+        }
+        if (allGrandSignShopLocationsToCorrespondingSignLocation.containsKey(blockLocation)) {
+            BlockLocation signLocation = allGrandSignShopLocationsToCorrespondingSignLocation.get(blockLocation);
+            return Optional.of(signLocationToGrandSignShopLocation.get(signLocation));
+        }
+        return Optional.empty();
     }
 
     @Override
