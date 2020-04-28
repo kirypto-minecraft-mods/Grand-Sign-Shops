f91dfa18ddcd1920b0701838f77bc3d150c0198e
kirypto <kirypto@gmail.com>
2020-04-27 20:23:40 -0600

    Uncomment PriceRange class

--- src/main/java/kirypto/grandsignshops/domain/PriceRange.java
+++ src/main/java/kirypto/grandsignshops/domain/PriceRange.java
@@ -1,23 +1,23 @@
 package kirypto.grandsignshops.domain;
 
 public class PriceRange {
-    // private final int low;
-    // private final int high;
-    //
-    // private PriceRange(int low, int high) {
-    //     this.low = low;
-    //     this.high = high;
-    // }
-    //
-    // public static PriceRange of(int low, int high) {
-    //     return new PriceRange(low, high);
-    // }
-    //
-    // public int getLow() {
-    //     return low;
-    // }
-    //
-    // public int getHigh() {
-    //     return high;
-    // }
+    private final int low;
+    private final int high;
+
+    private PriceRange(int low, int high) {
+        this.low = low;
+        this.high = high;
+    }
+
+    public static PriceRange of(int low, int high) {
+        return new PriceRange(low, high);
+    }
+
+    public int getLow() {
+        return low;
+    }
+
+    public int getHigh() {
+        return high;
+    }
 }
