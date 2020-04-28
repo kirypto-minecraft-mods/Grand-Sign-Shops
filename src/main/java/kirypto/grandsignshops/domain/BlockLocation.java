fbdf4051e0e6100dfd409298d36024365266f6e2
kirypto <kirypto@gmail.com>
2020-04-27 20:26:18 -0600

    Uncomment BlockLocation where possible

--- src/main/java/kirypto/grandsignshops/domain/BlockLocation.java
+++ src/main/java/kirypto/grandsignshops/domain/BlockLocation.java
@@ -1,70 +1,73 @@
 package kirypto.grandsignshops.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class BlockLocation {
-    //
-    // private final int dimension;
-    // private final int x;
-    // private final int y;
-    // private final int z;
-    //
-    // private BlockLocation(int dimension, int x, int y, int z) {
-    //     this.dimension = dimension;
-    //     this.x = x;
-    //     this.y = y;
-    //     this.z = z;
-    // }
-    //
+
+    private final int dimension;
+    private final int x;
+    private final int y;
+    private final int z;
+
+    private BlockLocation(int dimension, int x, int y, int z) {
+        this.dimension = dimension;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     // public static BlockLocation of(World world, BlockPos blockPos) {
     //     return new BlockLocation(world.provider.getDimension(), blockPos.getX(), blockPos.getY(), blockPos.getZ());
     // }
-    //
-    // @Override
-    // public boolean equals(Object obj) {
-    //     if (this == obj) {
-    //         return true;
-    //     }
-    //
-    //     if (obj == null || getClass() != obj.getClass()) {
-    //         return false;
-    //     }
-    //
-    //     BlockLocation that = (BlockLocation) obj;
-    //
-    //     return new EqualsBuilder()
-    //             .append(dimension, that.dimension)
-    //             .append(x, that.x)
-    //             .append(y, that.y)
-    //             .append(z, that.z)
-    //             .isEquals();
-    // }
-    //
-    // @Override
-    // public int hashCode() {
-    //     return new HashCodeBuilder(17, 37)
-    //             .append(dimension)
-    //             .append(x)
-    //             .append(y)
-    //             .append(z)
-    //             .toHashCode();
-    // }
-    //
-    // public int getDimension() {
-    //     return dimension;
-    // }
-    //
-    // public int getX() {
-    //     return x;
-    // }
-    //
-    // public int getY() {
-    //     return y;
-    // }
-    //
-    // public int getZ() {
-    //     return z;
-    // }
-    //
-    // public static BlockLocation shift(BlockLocation blockLocation, int dx, int dy, int dz) {
-    //     return new BlockLocation(blockLocation.dimension, blockLocation.x + dx, blockLocation.y + dy, blockLocation.z + dz);
-    // }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        BlockLocation that = (BlockLocation) obj;
+
+        return new EqualsBuilder()
+                .append(dimension, that.dimension)
+                .append(x, that.x)
+                .append(y, that.y)
+                .append(z, that.z)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(dimension)
+                .append(x)
+                .append(y)
+                .append(z)
+                .toHashCode();
+    }
+
+    public int getDimension() {
+        return dimension;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public static BlockLocation shift(BlockLocation blockLocation, int dx, int dy, int dz) {
+        return new BlockLocation(blockLocation.dimension, blockLocation.x + dx, blockLocation.y + dy, blockLocation.z + dz);
+    }
 }
